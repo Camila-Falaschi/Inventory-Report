0432cd43f35c93a7f3668830cb0cfcf1a396eeb2
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-11 13:15:03 -0300

    :white_check_mark: Chore: Implementing test for init method from Product class.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui /
+    product = Product(
+        1,
+        "Product Name",
+        "Company Name",
+        "11/03/2023",
+        "01/04/2024",
+        "ANYTHING123",
+        "Keep far from children and animals",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Product Name"
+    assert product.nome_da_empresa == "Company Name"
+    assert product.data_de_fabricacao == "11/03/2023"
+    assert product.data_de_validade == "01/04/2024"
+    assert product.numero_de_serie == "ANYTHING123"
+    assert (
+        product.instrucoes_de_armazenamento
+        == "Keep far from children and animals"
+    )
