67c79c8f77ec5a7758bf9f4609891b7c7c0bcc6f
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-11 18:09:43 -0300

    :white_check_mark: Chore: Implementing the "CCompleteReport" function to generate a complete version of the report.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, dict_list):
+        simple_report = super().generate(dict_list)
+        company_counter = Counter(
+            [item["nome_da_empresa"] for item in dict_list]
+        ).most_common()
+        companies_counter = ""
+        for company, quantity in company_counter:
+            companies_counter += f"- {company}: {quantity}\n"
+
+        return f"""{simple_report}
+Produtos estocados por empresa:
+{companies_counter}"""
