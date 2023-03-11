916da4fc343287a02f00b1ac16414dffd0620b20
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-11 20:34:18 -0300

    :white_check_mark: Chore: Implementing the "import_data" function to generate a report via JSON file.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -10,17 +11,16 @@ class Inventory:
             with open(path_file, mode="r") as file:
                 csv_file = csv.DictReader(file)
                 products_list = [product for product in csv_file]
-                if report_type == "simples":
-                    simple_report = SimpleReport()
-                    return simple_report.generate(products_list)
-                if report_type == "completo":
-                    complete_report = CompleteReport()
-                    return complete_report.generate(products_list)
+                return cls.generate_report(report_type, products_list)
+        if path_file.endswith(".json"):
+            with open(path_file) as file:
+                products_list = json.load(file)
+                return cls.generate_report(report_type, products_list)
 
-    # def generate_report(report_type, products_list):
-    #     if report_type == "simples":
-    #         simple_report = SimpleReport()
-    #         return simple_report.generate(products_list)
-    #     if report_type == "completo":
-    #         complete_report = CompleteReport()
-    #         return complete_report.generate(products_list)
+    def generate_report(report_type, products_list):
+        if report_type == "simples":
+            simple_report = SimpleReport()
+            return simple_report.generate(products_list)
+        if report_type == "completo":
+            complete_report = CompleteReport()
+            return complete_report.generate(products_list)
